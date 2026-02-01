5a64b83062feca8aa23d0c90330fc2bd4c034f3b
Trang <dothuytrang2312@gmail.com>
2026-02-01 02:02:07 +0100

    Delete unnecessary file in Exercise 2

--- Sprint-1/destructuring/exercise-2/exercise.test.js
+++
@@ -1,5 +0,0 @@
-const {hogwarts, gryffindorPeople} = require("./exercise")
-
-test("test to filter all Grynffindor people", () => {
-    expect(gryffindorPeople(hogwarts)).toEqual(["Harry Potter", "Ron Weasley", "Hermione Granger", "Minerva McGonagall", "Albus Dumbledore"])
-})
