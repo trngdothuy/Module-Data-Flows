1113076041245b0eb23d1205d68056c5a84b9731
Trang <dothuytrang2312@gmail.com>
2026-02-17 16:56:49 +0100

    Throw error for negative numbers in calculator.js and test

--- challenges/unit-testing/katas-tdd/calculator/calculator.js
+++ challenges/unit-testing/katas-tdd/calculator/calculator.js
@@ -4,10 +4,16 @@ function add(numbers) {
     } 
     const arr = numbers.split(",")
     let filterArr = arr.filter((num) => Number(num) <= 1000)
+    let negativeNumbers = arr.filter((num) => Number(num) < 0)
+
+    if (negativeNumbers.length > 0) {
+        throw new Error(`negatives not allowed: ${negativeNumbers.join(", ")}`)
+    }
 
     if (filterArr.length == 1) {
         return Number(filterArr[0])
-    } else {
+    } 
+    else {
         return filterArr.reduce((accumulator, value) => accumulator + Number(value), 0)
     }
 }
--- challenges/unit-testing/katas-tdd/calculator/calculator.test.js
+++ challenges/unit-testing/katas-tdd/calculator/calculator.test.js
@@ -17,5 +17,13 @@ test("return sum of more than 2 numbers from input", () => {
 })
 
 test("should ignore number bigger than 1000, return sum of input", () => {
-    expect(add("5,1,2,4,1001")).toEqual(12)
+    expect(add("5,1,2,4,10001")).toEqual(12)
+})
+
+test("should throw error for negative numbers", () => {
+    expect(() => add("1,4,-1")).toThrow('negatives not allowed: -1')
+})
+
+test("should ignore number bigger than 1000, return 0 if there is no other input", () => {
+    expect(add("10001")).toEqual(0)
 })
