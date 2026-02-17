943a8b71bcdea668a761e58ded1768cbd7d5c796
Trang <dothuytrang2312@gmail.com>
2026-02-17 17:10:35 +0100

    Complete password-verifier.js and tests

--- challenges/unit-testing/katas-tdd/password-verifier/password-verifier.js
+++ challenges/unit-testing/katas-tdd/password-verifier/password-verifier.js
@@ -1,7 +1,12 @@
 function verify(password) {
     if (password.length < 8) {
         return 'Password rejected'
-    }
+    } 
+    let passwordArr = password.split("")
+    if (!passwordArr.includes(/A-Z/) || !passwordArr.includes(/0-9/)) {
+        return 'Password rejected'
+    } 
+    return 'Password accepted'
 }
 
 module.exports = verify;
--- challenges/unit-testing/katas-tdd/password-verifier/password-verifier.test.js
+++ challenges/unit-testing/katas-tdd/password-verifier/password-verifier.test.js
@@ -3,3 +3,23 @@ const verify = require('./password-verifier')
 test("If the password is less than 8 characters, should return 'Password rejected'", () => {
     expect(verify("123")).toEqual('Password rejected')
 })
+
+test("If the password is null, should return 'Password rejected'", () => {
+    () => {
+    expect(verify("")).toEqual('Password rejected')}
+})
+
+test("If the password does not have at least 1 uppercase letter, return 'Password rejected'", () => {
+    () => {
+    expect(verify("abc")).toEqual('Password rejected')}
+})
+
+test("If the password does not have at least 1 number, return 'Password rejected'", () => {
+    () => {
+    expect(verify("abc")).toEqual('Password rejected')}
+})
+
+test("If the password have at least 1 number, 1 uppercase letter, and more than 7 letters, return 'Password rejected'", () => {
+    () => {
+    expect(verify("Abc1234")).toEqual('Password accepted')}
+})
